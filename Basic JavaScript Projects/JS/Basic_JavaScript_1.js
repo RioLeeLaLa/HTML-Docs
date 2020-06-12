50f740c1df3c7e046bfec5779822a3d14cf5ba02
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-12 02:33:24 -0700

    JS File Relocate

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+
+//Document Write
+document.write("<h1>Hello World!</h1><p>Have a nice day!</p>");
+
+//Font color, string values
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+
+
+
+
+
---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,5 @@
+//Type of Operator
+document.write(typeof "Word");
+
+//Output number
+document.write(typeof 3);
