3dca0a9912b8749cb548a3f3b448a0e8c922b41d
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-11 20:26:08 -0700

    java

--- Basic JavaScript Projects/Basic JavaScript/JS/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/Basic JavaScript/JS/Basic_JavaScript_6.js
@@ -37,3 +37,13 @@ function myFunction() {
     "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
     "manufactured in" + Erik.Vehicle_Year;
 }
+
+function count_Function() {
+    document.getElementById("Counting").innerHTML = Count () ;
+    function Count() {
+        var Starting_point =9;
+        function Plus_one {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
