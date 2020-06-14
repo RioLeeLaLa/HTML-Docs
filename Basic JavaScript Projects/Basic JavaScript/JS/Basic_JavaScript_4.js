f7d609da6a6a46c287130bb813ea61396f3bd6e7
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-13 22:17:33 -0700

    Step 140

--- Basic JavaScript Projects/Basic JavaScript/JS/Basic_JavaScript_4.js
+++
@@ -1,5 +0,0 @@
-//Type of Operator
-document.write(typeof "Word");
-
-//Output number
-document.write(typeof 3);
--- Basic JavaScript Projects/JS/Basic_JavaScript_7.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_7.js
@@ -1,30 +1,36 @@
-var X = 10;
-function Add_numbers_1() {
-    document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-    document.write(X + 100);
-}
-Add_numbers_1() ;
-Add_numbers_2() ;
 
-function Add_numbers_1() {
-var X = 10;
-document.write(20 + X + "<br>");
-}
-function Add_numbers_2() {
-    document.write(X + 100) ;
-}
-Add_numbers_1() ;
-Add_numbers_2() ;
 
-function Add_numbers_1() {
-    var X = 10;
-    console.log(15 + X) ;
+function get_Date() {
+    if (new Date() .getHours() < 23) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
 }
-function Add_numbers_2() {
-    console.log(X + 100) ;
-}
-Add_numbers_1() ;
-Add_numbers_2() ;
 
+function Age_Function() {
+    Age = document.getElementById("Age").nodeValue;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are old enough to vote!";
+    
+    
+}
+document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+function Time_function() {
+    var Time = new Date ().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
