a82a155071d9acfbf156b3f619c307c6bdf1b7a2
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-15 00:28:48 -0700

    For Review

--- Basic JavaScript Projects/JS/Basic_JavaScript_6.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_6.js
@@ -1,49 +1,51 @@
-document.write(Bigger = (5>1) ? "Left number bgger":"Right number is bigger");
 
-function Ride_Function () {
+function Ride_Function() {
     var Height, Can_ride;
     Height = document.getElementById("Height").value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride")innerHTML = Can_ride + " to ride. ";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
 }
 
-class Customer
-{
-    string FullName;
-    Boolean Active;
-    Customer(string name) {
-        Fullname = name;
-        Active = true;
-    }
-}
+function getFee(isMember) {
+    return (isMember ? '$2.00' : '$10.00');
+  }
+  
+  console.log(getFee(true));
+  // expected output: "$2.00"
+  
+  console.log(getFee(false));
+  // expected output: "$10.00"
+  
+  console.log(getFee(1));
+  // expected output: "$2.00"
 
-customer cust = new Customer("Brenda Smith");
 
-FullName = "Brenda Smith";
-Active = true;
-
-function Vehicle (Make, Model, Year, Color) {
+  //I cannot get this section to work. How is it supposed to look when performing correctly?
+function Vehicle(Make, Model, Year, Color) {
     this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model ;
+    this.Vehicle_Model = Model;
     this.Vehicle_Year = Year;
     this.Vehicle_Color = Color;
 }
-
-var Jack = new Vehicle("Dodge", "Viper, 2020, "Red");
-var Emily = new Vehicle("Jeep, Trail Hawk", 2019, "White and Black");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
 var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
 function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
-    "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
-    "manufactured in" + Erik.Vehicle_Year;
+    document.getElementById("New_and_This").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
 }
 
+//Also cannot get this section to work
 function count_Function() {
-    document.getElementById("Counting").innerHTML = Count () ;
+    document.getElementById("Counting").innerHTML = Count ();
     function Count() {
-        var Starting_point =9;
-        function Plus_one {Starting_point += 1;}
+        var Starting_point = 9;
+        function Plus_one() (Starting_point += 1;)
         Plus_one();
         return Starting_point;
     }
 }
+
+
+
