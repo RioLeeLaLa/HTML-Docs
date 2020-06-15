58c9302bdd1daf198c22ced302444cfb7a974588
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-14 23:33:42 -0700

    Page 116

--- Basic JavaScript Projects/JS/Basic_JavaScript_5.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -1,39 +1,13 @@
-document.write(Bigger = (5>1) ? "Left number bgger":"Right number is bigger");
+document.write(10==10)
 
-function Ride_Function () {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride")innerHTML = Can_ride + " to ride. ";
-}
+X = 10;
+Y = 10;
+document.write(X===Y);
 
-class Customer
-{
-    string FullName;
-    Boolean Active;
-    Customer(string name) {
-        Fullname = name;
-        Active = true;
-    }
-}
+A = "Hooray";
+B = "Hooray";
+document.write(A===B);
 
-customer cust = new Customer("Brenda Smith");
-
-FullName = "Brenda Smith";
-Active = true;
-
-function Vehicle (Make, Model, Year, Color) {
-    this.Vehicle_Make = Make;
-    this.Vehicle_Model = Model ;
-    this.Vehicle_Year = Year;
-    this.Vehicle_Color = Color;
-}
-
-var Jack = new Vehicle("Dodge", "Viper, 2020, "Red");
-var Emily = new Vehicle("Jeep, Trail Hawk", 2019, "White and Black");
-var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-function myFunction() {
-    document.getElementById("Keywords_and_Constructors").innerHTML = 
-    "Erik drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
-    "manufactured in" + Erik.Vehicle_Year;
+function not_Function () {
+    document.getElementById("Not").innerHTML = ! (20 > 10);
 }
