0813d6b126cb853fc20104d78d03d28b58556b70
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-25 21:49:10 -0700

    Page 59

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    alert("BOOM!");
+  }
+
+var Family = "Lee", First = "Regina", Dog = "Yes", Cat = "No";
+document.write(Family);
+
+var blues = " likes the color blue.";
+var blues = blues.fontcolor("blue");
+document.write(blues);
+
+document.write(3 +3 );
+
+//Please review
+function firstFunction() {
+    var str = "This text is green";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
