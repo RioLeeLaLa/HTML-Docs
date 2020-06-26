dcf7d0aba849ea13e79a21635d3457cfc6221d4d
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-26 13:23:08 -0700

    Page 86

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,16 @@
+window.alert(Math.random());
+
+//++ --
+var X = 5;
+X++;
+document.write(X);
+
+var X = 5.25;
+X--;
+document.write(X);
+
+// +, - , *, % 
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML == "1 plus 2, multiplied by 10, divided in half then subtracted by 5 equals" + simple_Math;
+}
