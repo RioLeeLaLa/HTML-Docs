4f56d062032c71e406dfaf97f58b579a4cb65e16
RioLeeLaLa <65325014+RioLeeLaLa@users.noreply.github.com>
2020-06-26 16:27:32 -0700

    Page 86

--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -9,8 +9,14 @@ var X = 5.25;
 X--;
 document.write(X);
 
-// +, - , *, % 
+// +, - , *
 function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 -5;
-    document.getElementById("Math").innerHTML == "1 plus 2, multiplied by 10, divided in half then subtracted by 5 equals" + simple_Math;
+    document.getElementById("Math1").innerHTML = "1 plus 2, multiplied by 10, divided in half then subtracted by 5 equals" + simple_Math;
+}
+
+//%
+function modulus_Operator() {
+    var simple_Math = 25 % 6;
+    document.getElementById("Math2").innerHTML = "When you divide 25 by 6 you have a remainder of " + simple_Math;
 }
